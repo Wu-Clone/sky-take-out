57409a2a3b70bc8575f63bfc13b9d2c82d829f08
Wu-Clone <wuli77917@gmail.com>
2024-09-30 15:38:34 +0800

    菜品管理 菜品分页查询

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,7 +1,20 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
+    /**
+     * 菜品保存
+     * @param dishDTO
+     */
     void saveWithFlavors(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
