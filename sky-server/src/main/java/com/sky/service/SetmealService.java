c4f82847ec214767fe937a103f188c2d0a9870cf
Wu-Clone <wuli77917@gmail.com>
2024-10-16 19:51:45 +0800

    实现业务：套餐管理 分页查询套餐

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,9 +1,13 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
 
     // 新增套餐，同时需要保存套餐和菜品的关联关系
     void saveWithDish(SetmealDTO setmealDTO);
+
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
