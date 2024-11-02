f737d00f1198a77d5d8634df7661cc791ac9b88d
Wu-Clone <wuli77917@gmail.com>
2024-11-02 18:16:28 +0800

    引入微信小程序

---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.sky.service.impl;
+
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+    public static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public User wxLogin(UserLoginDTO userLoginDTO) {
+
+
+
+        return null;
+    }
+}
