679b082a8c7951bb4c28854b46deaeeeb781f143
Wu-Clone <wuli77917@gmail.com>
2024-11-05 20:59:07 +0800

    客户端 完成购物车 删除商品，清空商品和查看商品

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,7 +1,20 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 public interface ShoppingCartService {
+    // 添加到购物车
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    // 添加到购物车
+    List<ShoppingCart> showShoppingCart();
+
+    //清空购物车所有商品
+    void cleanShoppingCart();
+
+    // 从购物车中删除
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -9,13 +9,16 @@ import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealMapper;
 import com.sky.mapper.ShoppingCartMapper;
 import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Autowired
@@ -60,20 +63,37 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCartMapper.insert(shoppingCart);
         }
     }
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        return shoppingCartMapper.list(ShoppingCart.builder()
+                .userId(BaseContext.getCurrentId())
+                .build());
+    }
+
+    @Override
+    public void cleanShoppingCart() {
+        shoppingCartMapper.deleteByUserId(BaseContext.getCurrentId());
+    }
+
+    @Override
+    public void subShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        // 既然能减 就一定存在
+        // 先查询自己购物车中的数据
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.list(shoppingCart);
+        ShoppingCart shoppingCartFromDB = shoppingCartList.get(0);
+        Integer number = shoppingCartFromDB.getNumber();
+        log.info("当前数据库内信息为:{}",shoppingCartFromDB);
+        log.info("当前数量为:{}",number);
+
+        if (number > 1) {
+            shoppingCartFromDB.setNumber(number - 1);
+            shoppingCartMapper.updateNumberById(shoppingCartFromDB);
+        } else {
+            shoppingCartMapper.deleteShoppingCart(shoppingCartFromDB.getId());
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
