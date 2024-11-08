c55765f579e15f6505f787ad588b29bc4839c20c
Wu-Clone <wuli77917@gmail.com>
2024-11-08 18:23:09 +0800

    定时任务完成

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     void insert(Orders order);
@@ -28,4 +31,7 @@ public interface OrderMapper {
      * @param orders
      */
     void update(Orders orders);
+
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrdertimeLT(Integer status, LocalDateTime orderTime);
 }
